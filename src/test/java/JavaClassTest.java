95aa887dff9910c90b885ee2a2bb4407aedb28d2
Heinrich Klobuczek <heinrich@mail.com>
2021-04-28 09:18:50 -0800

    Update

--- src/test/java/JavaClassTest.java
+++ src/test/java/JavaClassTest.java
@@ -1,6 +1,6 @@
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaClassTest {
     @Test
